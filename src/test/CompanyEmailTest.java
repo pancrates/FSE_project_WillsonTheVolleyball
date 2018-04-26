b439550892d4301586c6a44ee734d3f58698a6ad
pancrates <pancrates@protonmail.com>
2018-04-26 14:26:19 +0100

    Added Tests

---
+++ src/test/CompanyEmailTest.java
@@ -0,0 +1,46 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CompanyEmailTest {
+
+    @Test
+    void fromAddress() {
+    }
+
+    @Test
+    void toAddress() {
+    }
+
+    @Test
+    void subjectLine() {
+    }
+
+    @Test
+    void emailMessage() {
+    }
+
+    @Test
+    void setFrom() {
+    }
+
+    @Test
+    void setTo() {
+    }
+
+    @Test
+    void setSubject() {
+    }
+
+    @Test
+    void setMessage() {
+    }
+
+    @Test
+    void isValid() {
+    }
+
+    @Test
+    void toString() {
+    }
+}
---
+++ src/test/CompanyProjectTest.java
@@ -0,0 +1,68 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CompanyProjectTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getPID() {
+    }
+
+    @Test
+    void getPTitle() {
+    }
+
+    @Test
+    void setPTitle() {
+    }
+
+    @Test
+    void isContact() {
+    }
+
+    @Test
+    void addContact() {
+    }
+
+    @Test
+    void addEmail() {
+    }
+
+    @Test
+    void getEmailsForPhase() {
+    }
+
+    @Test
+    void getEmailsForPhase1() {
+    }
+
+    @Test
+    void nextPhase() {
+    }
+
+    @Test
+    void getPhaseByName() {
+    }
+
+    @Test
+    void getPhaseByID() {
+    }
+
+    @Test
+    void getProjectContacts() {
+    }
+
+    @Test
+    void toString() {
+    }
+}
